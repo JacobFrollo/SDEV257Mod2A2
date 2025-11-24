1f8f1ae380a93a08414c17d1cbd9cda61603f562
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-24 14:04:52 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform } from "react-native";
 import Planets from "./Planets";
 import Films from "./Films";
 import Spaceships from "./Spaceships";
+import PropTypes from "prop-types";
+import { Text, TextInput, View } from "react-native";
 
+//Creates navigation
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
+//Defines Input
+function Input(props) {
+    return (
+        <View style={styles.textInputContainer}>
+            <Text style={styles.textInputLabel}>{props.label}
+            </Text>
+            <TextInput style={styles.textInput} {...props} />
+        </View>
+    );
+}
+Input.propTypes = {
+    label: PropTypes.string,
+};
+
+//Collects Text
+export default function CollectingTextInput() {
+    return (
+        <View style={styles.container}>
+            <Input label="Basic Text Input:" />
+        </View>
+    );
+}
+
+//Builds the app
 export default function App() {
     return (
         <NavigationContainer>
