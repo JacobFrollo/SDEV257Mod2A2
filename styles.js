c4d06dfec3e87ad8f6683b6065e057b67521c30e
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-24 14:05:02 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -3,7 +3,23 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        backgroundColor: "ghostwhite",
+        padding: 10,
+        paddingTop: 80,
+    },
+
+    textInputContainer: {
+        alignSelf: "stretch",
+        marginBottom: 30,
+    },
+
+    textInputLabel: {
+        marginBottom: 4,
+    },
+
+    textInput: {
+        backgroundColor: "white",
+        height: 20,
+        fontSize: 11,
     },
 });
