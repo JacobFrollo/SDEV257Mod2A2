4154be25effd1409da5a28d740c654b7a163fe56
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-24 14:14:28 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
 import Planets from "./Planets";
 import Films from "./Films";
 import Spaceships from "./Spaceships";
